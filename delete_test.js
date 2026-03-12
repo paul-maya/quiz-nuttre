7bd300fe71afaeb50e081eac52d19e322775aafd
sarape-labs <ad.paulmaya@gmail.com>
2026-03-12 13:42:09 -0600

    Delete delete_test.js

--- delete_test.js
+++
@@ -1,8 +0,0 @@
-import db from './server/db.js';
-
-try {
-  db.prepare("DELETE FROM quizzes WHERE id = 'test-id-2'").run();
-  console.log("Deleted test-id-2");
-} catch (e) {
-  console.error("Error:", e.message);
-}
