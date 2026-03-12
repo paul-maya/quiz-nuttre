31e300b75a901729d27dc4812171922740719a84
sarape-labs <ad.paulmaya@gmail.com>
2026-03-12 13:43:57 -0600

    Delete test_alter.js

--- test_alter.js
+++
@@ -1,8 +0,0 @@
-import db from './server/db.js';
-
-try {
-  db.exec('ALTER TABLE quizzes ADD COLUMN slug2 TEXT UNIQUE');
-  console.log("Success");
-} catch (e) {
-  console.error("Error:", e.message);
-}
