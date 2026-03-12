86b55bad4e80bce1abddf75068c6edd9d21d9ed4
sarape-labs <ad.paulmaya@gmail.com>
2026-03-12 13:44:50 -0600

    Delete test_data.js

--- test_data.js
+++
@@ -1,8 +0,0 @@
-import db from './server/db.js';
-
-try {
-  const quizzes = db.prepare('SELECT * FROM quizzes').all();
-  console.log(quizzes);
-} catch (e) {
-  console.error("Error:", e.message);
-}
